d6e95a2832e1b76a25569ed0963214c161d1039b
Resg <sergey@panichev.su>
2019-09-25 22:04:20 +0300

    +авторизация

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import {startScreen} from "./components/StartScreen/StartScreen.js";
 import {SignUpScreen} from "./components/SignUpScreen/SignUpScreen.js";
+import {SignInScreen} from "./components/SignInScreen/SignInScreen.js";
 
 
 const application = document.getElementById('application');
 
 
-SignUpScreen(application);
+SignInScreen(application);
 //startScreen(application);
 
 
