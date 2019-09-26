72f4dc0f1262df4ed24065d73535f7f0bd7ed9bb
Resg <sergey@panichev.su>
2019-09-26 15:55:47 +0300

    переходы

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,10 @@
 import {ButtonComponent} from "../Button/Button.js";
 import {TextComponent} from "../TextComponent/Text.js";
 import {ImageComponent} from "../Image/Image.js";
+import {startScreen} from "../StartScreen/StartScreen.js";
+import {SignUpScreen} from "../SignUpScreen/SignUpScreen.js";
+import {SignInScreen} from "../SignInScreen/SignInScreen.js";
+
 
 
 export class HeaderComponent {
@@ -11,15 +15,43 @@ export class HeaderComponent {
 
     render() {
         const head = document.createElement('header');
+        const functions = {
+            start: startScreen,
+            signUp: SignUpScreen,
+            signIn: SignInScreen,
+            //profile: createProfile,
+            //about: null,
+        };
         this._parent.appendChild(head);
-        const text = new TextComponent({tag: "h1", class: "", text: "Online Poker Game"});
-        const chip = new ImageComponent({source: "./assets/gold_fishka.jpg", class: "chip"});
-        const signInButton = new ButtonComponent({button: 'class="logButton"', text:'Sign in'});
+        const text = new TextComponent({
+            tag: "h1",
+            class: "",
+            text: "Online Poker Game"
+        });
+        const chip = new ImageComponent({
+            source: "./assets/gold_fishka.jpg",
+            class: "chip",
+            section: "start"
+        });
+        const signInButton = new ButtonComponent({
+            href: '/SignIn',
+            text:'Sign in',
+            section: "signIn"});
         head.innerHTML += signInButton.render();
-        const signUpButton = new ButtonComponent({button: 'class="logButton"', text:'Sign up'});
+        const signUpButton = new ButtonComponent({
+            href: '/SignUp',
+            text:'Sign up',
+            section: "signUp"});
         head.innerHTML += signUpButton.render();
         head.innerHTML += chip.render();
         head.innerHTML += text.render();
+        head.addEventListener('click', evt => {
+            const {target} = evt;
+            if ((target instanceof HTMLButtonElement) || (target instanceof HTMLImageElement)) {
+                evt.preventDefault();
+                functions[target.dataset.section](application);
+            }
+        });
     }
 }
 
--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import {startScreen} from "./components/StartScreen/StartScreen.js";
 import {SignUpScreen} from "./components/SignUpScreen/SignUpScreen.js";
 import {SignInScreen} from "./components/SignInScreen/SignInScreen.js";
+import {HeaderComponent} from "./components/Header/Header.js";
 
 
 const application = document.getElementById('application');
 
-
-//startScreen(application);
-SignInScreen(application);
+startScreen(application);
+//SignInScreen(application);
 //SignUpScreen(application);
 
 
