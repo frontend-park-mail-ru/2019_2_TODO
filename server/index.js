428ee0d7ab8b98d19513ea577bdf5224938bedfd
Resg <sergey@panichev.su>
2019-09-29 21:04:31 +0300

    eslint

--- server/index.js
+++ server/index.js
@@ -1,20 +1,19 @@
 'use strict'
 
-const express = require('express');
-const body = require('body-parser');
-const cookie = require('cookie-parser');
-const morgan = require('morgan');
-const uuid = require('uuid/v4');
-const path = require('path');
-const app = express();
+const express = require('express')
+const body = require('body-parser')
+const cookie = require('cookie-parser')
+const morgan = require('morgan')
+const path = require('path')
+const app = express()
 
-app.use(morgan('dev'));
-app.use(express.static(path.resolve(__dirname, '..', 'src')));
-app.use(body.json());
-app.use(cookie());
+app.use(morgan('dev'))
+app.use(express.static(path.resolve(__dirname, '..', 'src')))
+app.use(body.json())
+app.use(cookie())
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 80
 
 app.listen(port, () => {
-    console.log(`Server listening port ${port}`);
-});
+  console.log(`Server listening port ${port}`)
+})
