59fc4b08260b43e0a9caf2bb77faeeb5e72f43d0
Resg <sergey@panichev.su>
2019-09-30 16:32:36 +0300

    валидация

--- src/components/SignUpScreen/SignUpScreen.js
+++ src/components/SignUpScreen/SignUpScreen.js
@@ -62,6 +62,11 @@ export const SignUpScreen = (application) => {
     const email = form.elements.email.value
     const password = form.elements.password
     const passwordRepeat = form.elements.passwordRepeat
+    const reg = /\w+@\w+/
+    if (email.search(reg) === -1) {
+      EmailInput.error('EMAIL_FORMAT', form)
+      return
+    }
     if (password.value !== passwordRepeat.value) {
       PassRepeat.error('PASSWORDS_MATCH', form)
       return
@@ -70,12 +75,6 @@ export const SignUpScreen = (application) => {
       PassInput.error('PASSWORD_LENGTH', form)
       return
     }
-    const reg = /\w+@\w+/
-    if (email.search(reg) === -1) {
-      EmailInput.error('EMAIL_FORMAT', form)
-      return
-    }
-
     AjaxModule._fetchPost(
       'http://93.171.139.196:780/signup/',
       JSON.stringify({
