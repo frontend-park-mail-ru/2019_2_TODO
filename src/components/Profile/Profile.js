97b4a54df833d99b27532f1c4d4ab3455814097f
Resg <sergey@panichev.su>
2019-10-07 15:49:39 +0300

    reg

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -57,7 +57,7 @@ export const renderProfile = (
         form.innerHTML += closeButton.render();
         const avatar = new ImageComponent({
           class: 'chip',
-          source: 'http://93.171.139.195:780/images',
+          source: 'http://93.171.139.196:780/images',
         });
         const avatarInput = new InputComponent({
           type: 'file',
