1cbf1dd8be8a623019f7af12798b7574ede7ec4a
Resg <sergey@panichev.su>
2019-10-10 19:08:11 +0300

    redirect

--- server/index.js
+++ server/index.js
@@ -11,18 +11,18 @@ const cors = require('cors');
 const rootDir = path.resolve(__dirname, '..', 'src');
 
 app.use(morgan('dev'));
-app.use(express.static(rootDir));
+
 app.use(body.json());
 app.use(cookie());
-app.use(fallback('index.html', {root: rootDir}));
-
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://93.171.139.195:780/logout/");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.set("Access-Control-Allow-Origin", "http://93.171.139.195:780/logout/");
+  res.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.set('Access-Control-Allow-Credentials', true);
   next();
 });
+app.use(express.static(rootDir));
+app.use(fallback('index.html', {root: rootDir}));
 
 const port = process.env.PORT || 80;
 
