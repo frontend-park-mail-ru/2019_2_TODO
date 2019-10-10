ba5f185ec97ed1992b330f74a0b1bc5acbb900bb
Resg <sergey@panichev.su>
2019-10-10 18:16:15 +0300

    ProfileView

--- server/index.js
+++ server/index.js
@@ -15,7 +15,10 @@ app.use(express.static(rootDir));
 app.use(body.json());
 app.use(cookie());
 app.use(fallback('index.html', {root: rootDir}));
-app.use(cors());
+app.use(cors({
+  origin: 'http://93.171.139.196:780/',
+  optionsSuccessStatus: 200
+}));
 
 
 const port = process.env.PORT || 80;
