f0ed5f45b77e2acc30fa190b4b818758d7889c4c
Resg <sergey@panichev.su>
2019-11-08 14:22:28 +0300

    fix

--- src/components/viewes/Profile/Change/ChangeProfile.js
+++ src/components/viewes/Profile/Change/ChangeProfile.js
@@ -40,105 +40,114 @@ export default class ChangeProfileView extends BaseView {
         })
         .then((resT) => {
           console.log(resT);
-          const form = document.createElement('form');
-          application.innerHTML = '';
-          application.appendChild(form);
-          form.className = 'profileForm';
-          const closeButton = new TextComponent({
-            tag: 'a',
-            id: 'cl',
-            href: '/profile',
-            class: 'button profileForm__button',
-            text: 'close',
+          const prof = new profileChange({
+            username: JSON.parse(resT).username,
+            avatar: JSON.parse(resT).image
           });
-          form.innerHTML += closeButton.render();
-          const avatar = new ImageComponent({
-            class: 'profileForm__avatar',
-            source: JSON.parse(resT).image,
-          });
-          const avatarInput = new InputComponent({
-            type: 'file',
-            class: 'profileForm__input',
-            id: 'avatarInput',
-            placeholder: JSON.parse(resT).image,
-          });
-          avatarInput.render();
-          form.innerHTML += avatar.render();
-          form.innerHTML += avatarInput.render();
-          const changeAvButton = new ButtonComponent({
-            type: 'submit',
-            class: 'profileForm__button',
-            id: 'changeAv',
-            text: 'Upload avatar',
-          });
-          form.innerHTML += changeAvButton.render();
-
-          const nickname = new InputComponent({
-            class: 'profileForm__input',
-            id: 'nick',
-            placeholder: JSON.parse(resT).username,
-          });
-          form.innerHTML += nickname.render();
-          const password = new InputComponent({
-            class: 'profileForm__input',
-            id: 'pass',
-            type: 'password',
-            placeholder: 'new password',
-          });
-          const passwordRepeat = new InputComponent({
-            class: 'profileForm__input',
-            id: 'passr',
-            type: 'password',
-            placeholder: 'repeat Passwor',
-          });
-
-          form.innerHTML += password.render();
-          form.innerHTML += passwordRepeat.render();
-          const changeButton = new ButtonComponent({
-            id: 'changeNP',
-            type: 'submit',
-            class: 'profileForm__button',
-            text: 'change',
-          });
-          form.innerHTML += changeButton.render();
+          this.el.innerHTML = prof.render();
+          // const form = document.createElement('form');
+          // application.innerHTML = '';
+          // application.appendChild(form);
+          // form.className = 'profileForm';
+          // const closeButton = new TextComponent({
+          //   tag: 'a',
+          //   id: 'cl',
+          //   href: '/profile',
+          //   class: 'button profileForm__button',
+          //   text: 'close',
+          // });
+          // form.innerHTML += closeButton.render();
+          // const avatar = new ImageComponent({
+          //   class: 'profileForm__avatar',
+          //   source: JSON.parse(resT).image,
+          // });
+          // const avatarInput = new InputComponent({
+          //   type: 'file',
+          //   class: 'profileForm__input',
+          //   id: 'avatarInput',
+          //   placeholder: JSON.parse(resT).image,
+          // });
+          // avatarInput.render();
+          // form.innerHTML += avatar.render();
+          // form.innerHTML += avatarInput.render();
+          // const changeAvButton = new ButtonComponent({
+          //   type: 'submit',
+          //   class: 'profileForm__button',
+          //   id: 'changeAv',
+          //   text: 'Upload avatar',
+          // });
+          // form.innerHTML += changeAvButton.render();
+          //
+          // const nickname = new InputComponent({
+          //   class: 'profileForm__input',
+          //   id: 'nick',
+          //   placeholder: JSON.parse(resT).username,
+          // });
+          // form.innerHTML += nickname.render();
+          // const password = new InputComponent({
+          //   class: 'profileForm__input',
+          //   id: 'pass',
+          //   type: 'password',
+          //   placeholder: 'new password',
+          // });
+          // const passwordRepeat = new InputComponent({
+          //   class: 'profileForm__input',
+          //   id: 'passr',
+          //   type: 'password',
+          //   placeholder: 'repeat Passwor',
+          // });
+          //
+          // form.innerHTML += password.render();
+          // form.innerHTML += passwordRepeat.render();
+          // const changeButton = new ButtonComponent({
+          //   id: 'changeNP',
+          //   type: 'submit',
+          //   class: 'profileForm__button',
+          //   text: 'change',
+          // });
+          // form.innerHTML += changeButton.render();
 
           // const profile = new ChangeProfile(context);
           // application.innerHTML = profile.render();
-          const avButton = document.getElementById('changeAv');
-          const npButton = document.getElementById('changeNP');
-          avButton.addEventListener('click', (evt) => {
+          // const avButton = document.getElementById('changeAv');
+          // const npButton = document.getElementById('changeNP');
+
+          this.el.addEventListener('click', (evt) => {
             evt.preventDefault();
-            const av = form.elements.avatarInput;
-            const data = new FormData();
-            data.append('image', av.files[0]);
-            AjaxModule.postAvatar(application, data);
-          });
-          npButton.addEventListener('click', (evt) => {
-            evt.preventDefault();
-            const nick = form.elements.nick.value;
-            const pass = form.elements.pass.value;
-            const passRepeat = form.elements.passr.value;
-            if (pass !== passRepeat) {
-              password.error('PASSWORDS_MATCH', form);
-              return;
+            if (evt.target.id === 'changeAv'){
+              const av = document.getElementById('avatarInput');
+              const data = new FormData();
+              data.append('image', av.files[0]);
+              AjaxModule.postAvatar(application, data);
             }
-            if ((pass.length < 5)&&(pass.length > 0)) {
-              password.error('PASSWORD_LENGTH', form);
-              return;
+            if (evt.target.id === 'changeNP'){
+              evt.preventDefault();
+              const form = document.getElementById('inputContainer');
+              const nick = form.elements.nick.value;
+              const pass = form.elements.pass.value;
+              const passRepeat = form.elements.passr.value;
+              if (pass !== passRepeat) {
+                password.error('PASSWORDS_MATCH', form);
+                return;
+              }
+              if ((pass.length < 5)&&(pass.length > 0)) {
+                password.error('PASSWORD_LENGTH', form);
+                return;
+              }
+              AjaxModule.fetchPost(
+                  'http://93.171.139.196:780/signin/profile/',
+                  JSON.stringify({
+                    username: nick,
+                    password: pass,
+                  })
+              )
+                  .then((res) => {
+                    if (res.status === 200) {
+                      console.log(res);
+                      window.router.open('/');
+                    }
+                  });
             }
-            AjaxModule.fetchPost(
-                'http://93.171.139.196:780/signin/profile/',
-                JSON.stringify({
-                  username: nick,
-                  password: pass,
-                })
-            )
-                .then((res) => {
-                  if (res.status === 200) {
-                    console.log(res);
-                    window.router.open('/');
-                  }
-                });
           });
         });
   }
