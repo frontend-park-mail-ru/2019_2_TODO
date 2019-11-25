77779bf7807cb8e0d996ee0fe51e8d39cf985549
Resg <sergey@panichev.su>
2019-11-25 17:05:09 +0300

    tables

--- src/viewes/BaseView/BaseView.js
+++ src/viewes/BaseView/BaseView.js
@@ -8,18 +8,21 @@ export default class BaseView {
     this.el = element;
 
     this.el.dataset.view = this.constructor.name;
-    this.el.hidden = false;
+    // this.el.hidden = false;
+    this.isActive = false;
   }
 
   get active() {
-    return !this.el.hidden;
+    return this.isActive;
   }
 
   hide() {
+    this.isActive = false;
     this.el.innerHTML = '';
   }
 
   show() {
+    this.isActive = true;
     this.render();
   }
 
