b58ace5dc39ff95104423d4e3940e722231827cc
Resg <sergey@panichev.su>
2019-12-15 16:18:19 +0300

    scoreboard

--- src/components/ScoreBoard/ScoreBoard.js
+++ src/components/ScoreBoard/ScoreBoard.js
@@ -21,7 +21,7 @@ export default class ScoreBoard extends BaseComponent {
    */
   render() {
     this.compile();
-    AjaxModule.fetchGet('/scoreboard/')
+    AjaxModule.fetchGet('https://pokertodo.online:743/leaderboard/')
         .then((res)=>{
           return res.text();
         })
